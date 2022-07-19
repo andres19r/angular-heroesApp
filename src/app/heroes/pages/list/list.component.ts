c95a5792832ca8583bdf47b100fe04a594ed58ea
Andres Rivero <andres19rivero@gmail.com>
2022-07-18 20:03:58 -0400

    hero interface & list the heroes

--- src/app/heroes/pages/list/list.component.ts
+++ src/app/heroes/pages/list/list.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor(private heroesService: HeroesService) { }
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
     this.heroesService.getHeroes()
-      .subscribe(resp => console.log(resp));
+      .subscribe((resp) => (this.heroes = resp));
   }
-
 }
