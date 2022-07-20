c463962df1262f97537ecb95edf48dcfbea86d41
Andres Rivero <andres19rivero@gmail.com>
2022-07-20 19:14:20 -0400

    hero page with request by id

--- src/app/heroes/pages/hero/hero.component.ts
+++ src/app/heroes/pages/hero/hero.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/heroes.interface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent implements OnInit {
+  hero!: Hero;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe( ({ id }) => {
-      console.log(id)
-    })
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => (this.hero = hero));
   }
-
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -13,4 +13,8 @@ export class HeroesService {
   getHeroes(): Observable<Hero[]>  {
     return this.http.get<Hero[]>('http://localhost:3000/heroes')
   }
+
+  getHeroById(id: string): Observable<Hero>{
+    return this.http.get<Hero>(`http://localhost:3000/heroes/${id}`)
+  }
 }
