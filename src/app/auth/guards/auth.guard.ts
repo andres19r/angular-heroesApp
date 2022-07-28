43dbdf10b66e764673f8eefb1bdaf446b1bd6ccb
Andres Rivero <andres19rivero@gmail.com>
2022-07-27 22:07:39 -0400

    Saving the user session

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -4,11 +4,11 @@ import {
   CanActivate,
   CanLoad,
   Route,
+  Router,
   RouterStateSnapshot,
   UrlSegment,
-  UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -16,23 +16,31 @@ import { AuthService } from '../services/auth.service';
 })
 export class AuthGuard implements CanLoad, CanActivate {
   
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.authService.auth.id) {
-      return true;
-    }
-    return false;
+    return this.authService.verifyAuthentication()
+      .pipe(
+        tap(isAuthenticated => {
+          if(!isAuthenticated) {
+            this.router.navigate(['./auth/login'])
+          }
+        })
+      )
   }
   canLoad(
     route: Route,
     segments: UrlSegment[]
   ): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.authService.auth.id) {
-      return true;
-    }
-    return false;
+    return this.authService.verifyAuthentication()
+      .pipe(
+        tap(isAuthenticated => {
+          if(!isAuthenticated) {
+            this.router.navigate(['./auth/login'])
+          }
+        })
+      )
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs';
+import { map, Observable, tap, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
 
@@ -17,12 +17,25 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
+  verifyAuthentication(): Observable<boolean> {
+    if(!localStorage.getItem('token')) {
+      return of(false)
+    }
+    return this.http.get<Auth>(`${this.baseUrl}/users/1`)
+      .pipe(
+        map(auth => {
+          this._auth = auth
+          return true
+        })
+      )
+  }
+
   login() {
     return this.http
       .get<Auth>(`${this.baseUrl}/users/1`)
       .pipe(
         tap((auth) => (this._auth = auth)),
-        tap((auth) => localStorage.setItem('id', auth.id))
+        tap((auth) => localStorage.setItem('token', auth.id))
       );
   }
 
